f881a6d0d64fa11384234083354c7f14f101fcb9
Øyvind Stokke <oyvsto@gmail.com>
2020-05-27 19:26:19 +0200

    Initial commit

---
+++ _TestApp/Form1.cs
@@ -0,0 +1,33 @@
+﻿using msg.winforms;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace _TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // will be shown until disposed
+            using (Cursors.WaitCursor.ShowUntilDisposed())
+                Thread.Sleep(1000);
+
+            // no need for using statement, wait cursor will be shown until action is performed
+            Cursors.WaitCursor.ShowWhile(() => { Thread.Sleep(1000); });
+
+            var i = Cursors.WaitCursor.ShowWhile(() => sleepAndReturn(1000));
+        }
+
+        private int sleepAndReturn(int sleepTime)
+        {
+            Thread.Sleep(sleepTime);
+            return sleepTime;
+        }
+    }
+}
---
+++ msg.winforms/CursorWait.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace msg.winforms
+{
+    public static class MsgCursor
+    {
+        /// <summary>
+        /// Creates a disposible object that will set the given cursor on initialization and reset to default when disposed.
+        /// </summary>
+        public static IDisposable ShowUntilDisposed(this Cursor cursor)
+        {
+            return new CursorWait(cursor);
+        }
+
+        /// <summary>
+        /// Sets the given cursor as default while the given action is performed.
+        /// </summary>
+        public static void ShowWhile(this Cursor cursor, Action action)
+        {
+            using (cursor.ShowUntilDisposed())
+                action();
+        }
+
+        /// <summary>
+        /// Sets the given cursor as default while the given action is performed.
+        /// </summary>
+        public static T ShowWhile<T>(this Cursor cursor, Func<T> action)
+        {
+            using (cursor.ShowUntilDisposed())
+                return action();
+        }
+    }
+
+    internal class CursorWait : IDisposable
+    {     
+        public CursorWait(Cursor cursor)
+        {
+            Cursor.Current = cursor;
+        }
+
+        public void Dispose()
+        {
+            Cursor.Current = Cursors.Default;
+            Application.UseWaitCursor = false;
+        }
+    }
+}
